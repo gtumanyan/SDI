cb191908eaee1157043a0c992a19b2c8a50e4768
WindR <gtumanyan@mail.ru>
2021-07-10 19:05:56 +0300

    Update libtorrent project to VS 17, disable encryption, mutable-torrents and i2p support. Disable smaller type check in main project

--- project/vs_libtorrent/cmake(run from x64_x86 Cross Tools).cmd
+++ project/vs_libtorrent/cmake(run from x64_x86 Cross Tools).cmd
@@ -1,2 +1,2 @@
-cmake -DCMAKE_CXX_STANDARD=14 ..\..\ext\libtorrent -DBoost_USE_STATIC_LIBS=ON -A Win32 -B "Win32" -Dstatic_runtime=ON -DBUILD_SHARED_LIBS=Off -Dbuild_examples=On
-cmake -DCMAKE_CXX_STANDARD=14 ..\..\ext\libtorrent -DBoost_USE_STATIC_LIBS=ON -A x64 -B "x64" -Dstatic_runtime=ON -DBUILD_SHARED_LIBS=Off -Dbuild_examples=On
+cmake -DCMAKE_CXX_STANDARD=14 ..\..\ext\libtorrent -DBoost_USE_STATIC_LIBS=ON -G"Visual Studio 17 2022" -AWin32 -BWin32 -Dstatic_runtime=ON -DBUILD_SHARED_LIBS=Off -Dbuild_examples=On -Dencryption=Off -Dextensions=Off -Dmutable-torrents=Off -Di2p=Off
+cmake -DCMAKE_CXX_STANDARD=14 ..\..\ext\libtorrent -DBoost_USE_STATIC_LIBS=ON -G"Visual Studio 17 2022" -Ax64 -Bx64 -Dstatic_runtime=ON -DBUILD_SHARED_LIBS=Off -Dbuild_examples=On -Dencryption=Off -Dextensions=Off -Dmutable-torrents=Off -Di2p=Off
