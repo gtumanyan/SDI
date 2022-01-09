6bd795e5cb548173136a151800e2ad2a788cda48
WindR <gtumanyan@mail.ru>
2022-01-09 14:15:08 +0300

    fix version information

--- src/VersionEx.h
+++ src/VersionEx.h
@@ -8,12 +8,12 @@
 #define SAPPNAME "SDI"
 #define VERSION_MAJOR 2
 #define VERSION_MINOR 22
-#define VERSION_REV 106
-#define VERSION_BUILD 1
+#define VERSION_REV 109
+#define VERSION_BUILD 2
 #define WEBP_VER 1.2.2
 #define TORRENT_VER 1.2.15
 #define BOOST_VER 1.79.0
 #define SZIP_VER 21.07
 
-#define VERSION_STATUS "alpha"
+#define VERSION_STATUS "beta"
 #define VERSION_COMMIT_ID "aegis"
