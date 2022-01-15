2d2c9f946b6429c4a2f71a92c05cd5277878f6ee
WindR <gtumanyan@mail.ru>
2022-01-15 13:12:50 +0300

    update images converter

--- res/orig/convert_all.bat
+++ res/orig/convert_all.bat
@@ -1,4 +1,4 @@
 mkdir outpng
 mkdir outwebp
-for /F %%i in ('dir /b *.webp') do dwebp.exe %%i -o outpng\%%i.png
-for /F %%i in ('dir /b *.png')  do cwebp.exe %%i -o outwebp\%%i.webp -lossless
+for /F %%i in ('dir /b *.webp') do ..\..\ext\libwebp\output\release-static\x64\bin\dwebp.exe %%i -o outpng\%%~ni.png
+for /F %%i in ('dir /b *.png')  do ..\..\ext\libwebp\output\release-static\x64\bin\cwebp.exe %%i -o outwebp\%%~ni.webp -lossless
