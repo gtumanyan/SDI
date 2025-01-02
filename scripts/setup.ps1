c9cf25959e5f9117068744773e7f8780e06f1379
WindR <gtumanyan@mail.ru>
2025-01-02 17:56:17 +0300

    Fix init script

--- scripts/setup.ps1
+++ scripts/setup.ps1
@@ -1,4 +1,4 @@
-## check admin rights
+## Check admin rights
 If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
     Write-Warning "You do not have Administrator rights to run this script!`nPlease re-run this script as an Administrator!"
     Break
@@ -6,18 +6,11 @@ If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdent
 
 ## Install Git
 winget install Git.Git -i
-Write-Host -NoNewLine "`n Environment config complete, Press any key to continue...";
-$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
-
-# install nerd fonts
-git clone --filter=blob:none --sparse https://github.com/ryanoasis/nerd-fonts "$env:USERPROFILE/Downloads/nerd-fonts"
-
-Invoke-Expression "$env:USERPROFILE/Downloads/nerd-fonts/install.ps1"
 
 ## Install VS 2022
 # https://learn.microsoft.com/en-us/visualstudio/install/workload-component-id-vs-community?view=vs-2022&preserve-view=true#desktop-development-with-c
 Write-Host "`n Install VS 2022" -ForegroundColor Green
-winget install Microsoft.VisualStudio.2022.Community.Preview --override "install --config $PSScriptRoot\..\vs2022\.vsconfig --lang en-US --passive --wait --allowUnsignedExtensions"
+winget install Microsoft.VisualStudio.2022.Community.Preview --override "install --config $PSScriptRoot\..\vs2022\.vsconfig --locale en-US --passive --wait --allowUnsignedExtensions"
 
 ## Start SDI Project
-$PSScriptRoot\..\vs2022\SDI.sln
+Start-Process "$PSScriptRoot\..\vs2022\SDI.sln"
